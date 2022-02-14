6790e8c510c88b3438b09d9c99aef55f95bee90a
Ilya Aleksandrov <i.m.alexandrov@yandex.ru>
2022-02-14 20:05:20 +0500

    camelCase

--- src/Task2.java
+++ src/Task2.java
@@ -1,17 +1,17 @@
 public class Task2 {
     public static void main(String[] args) {
         int currentAccount = 100; // - Сумма начального счета
-        int TopUpAmount = 20000; // - Сумма пополнения
-        int TotalAmount = (TopUpAmount >= 1000) ? TopUpAmount / 100 + TopUpAmount + currentAccount : TopUpAmount + currentAccount; // - Итоговая сумма после пополнения
+        int topUpAmount = 20000; // - Сумма пополнения
+        int totalAmount = (topUpAmount >= 1000) ? topUpAmount / 100 + topUpAmount + currentAccount : topUpAmount + currentAccount; // - Итоговая сумма после пополнения
         //
         //                                        ------ Вариант с if else ------
         //
-        //int TotalAmount;
-        //if (TopUpAmount >= 1000) {
-        //    TotalAmount = TopUpAmount / 100 + TopUpAmount + currentAccount;
+        //int totalAmount;
+        //if (topUpAmount >= 1000) {
+        //    totalAmount = topUpAmount / 100 + topUpAmount + currentAccount;
         //} else {
-        //    TotalAmount = TopUpAmount + currentAccount;
+        //    totalAmount = topUpAmount + currentAccount;
         //}
-        System.out.println(TotalAmount); // - Вывод итоговой суммы счета после пополнения
+        System.out.println(totalAmount); // - Вывод итоговой суммы счета после пополнения
     }
 }
